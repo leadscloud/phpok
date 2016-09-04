8e7deb43de4a9b1e590f2c734757c76719c5d017
wanlianbo <lianbo.wan@gmail.com>
2016-09-04 14:47:20 +0800

    修改忽略文件

--- data/tpl_c/1inc/pagelist.php
+++
@@ -1,5 +0,0 @@
-<?php if(!defined('PHPOK_SET')){die('<h3>Error...</h3>');}?><div class="pagelist">
-	<?php $_i=0;$pagelist=(is_array($pagelist))?$pagelist:array();foreach($pagelist AS  $key=>$value){$_i++; ?>
-	<a href="<?php echo $value[url];?>"<?php if($value[status]){?> class="hover"<?php } ?>><?php echo $value[name];?></a>
-	<?php } ?>
-</div>
